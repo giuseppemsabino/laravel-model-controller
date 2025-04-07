c50412afeb962a04147ead82efe09edcf46ec9da
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-07 18:00:19 +0200

    done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,26 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movies</title>
-</head>
-<body>
-    <h1>Movies</h1>
-    <ul>
+@extends('layouts.master')
+@section('content')
+
+<div class="container my-3">
+    <section class="mb-5 gap-3 row">
+        
+
         @foreach ($movies as $movie)
-        <li>
-            <h1>{{$movie->title}}</h1>
-        </li>
+            <div class="card  col-3 movie-card">
+                <div class="card-body">
+
+                    <h4>{{$movie->title}}</h4>
+                    <p><strong>({{$movie->original_title}})</strong></p>
+                    <p><strong>Data di uscita:</strong> {{$movie->date}}</p>
+                </div>
+                <div class="card-footer bg-primary text-white">
+                    <p><strong>Voto:</strong> {{$movie->vote}}</p>
+                </div>
+
+            </div>
         @endforeach
-    </ul>
-</body>
-</html>
+
+    </section>
+
+</div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="container">
+
+        <h1>I miei film su Laravel</h1>
+
+    </div>
+</header>
